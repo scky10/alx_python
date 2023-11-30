f2670b236afc665fbc366300cf2967dd030bccf1
scky10 <yf.khalil10@gmail.com>
2023-11-30 20:12:12 +0100

    2

---
+++ python-functions/1-power.py
@@ -0,0 +1,9 @@
+def pow(a, b):
+    result = 1
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result /= a
+    return result
