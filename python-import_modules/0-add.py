909d0a40db001b21f4b255b3de7b53c3c9a2abd1
scky10 <yf.khalil10@gmail.com>
2023-12-07 22:20:10 +0100

    tre

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,11 @@
+import importlib
+
 if __name__ == "__main__":
     a = 1
     b = 2
 
-    add_module = __import__('add_0')
+    add_module = importlib.import_module('add_0')
     add_function = getattr(add_module, 'add')
-    result = add_module.add(a, b)
+    result = add_function(a, b)
     output = "{} + {} = {}\n".format(a, b, result)
     print(output)
