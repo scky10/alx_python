809784160310aa714df80e94d552af5f8fbb6914
scky10 <yf.khalil10@gmail.com>
2024-01-28 17:26:04 +0100

    up

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,8 +6,8 @@ last_digit = int(number_str[-1])
 if number < 0 :
 	last_digit = last_digit * (-1)
 if last_digit > 5:
-    print("Last digit of " + str(number) + " is " + number_str[-1] + "and is greater than 5")
+    print("Last digit of " + str(number) + " is " + str(last_digit) + "and is greater than 5")
 elif last_digit < 6 and last_digit != 0 :
-    print("Last digit of " + str(number) + " is " + number_str[-1] + " and is less than 6 and not 0")
+    print("Last digit of " + str(number) + " is " + str(last_digit) + " and is less than 6 and not 0")
 else:
-    print("Last digit of " + str(number) + " is " + number_str[-1] + " and is 0")
+    print("Last digit of " + str(number) + " is " + str(last_digit) + " and is 0")
